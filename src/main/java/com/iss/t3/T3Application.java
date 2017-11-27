e3d028d9c4ceb2d30fb5037d0fb7f15a03a6eb7c
Vishal Devmurari <tech.86ishal@gmail.com>
2017-11-28 00:43:31 +0530

    Added src folder

---
+++ src/main/java/com/iss/t3/T3Application.java
@@ -0,0 +1,12 @@
+package com.iss.t3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T3Application.class, args);
+	}
+}
